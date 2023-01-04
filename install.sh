35c230c5249ce7a83098d3dcaf351b7aa23a9130
Mohammed Benkhattab <mbenkhat@e2r12p5.1337.ma>
2023-01-04 11:31:27 +0100

    install.sh code optimisation

--- install.sh
+++ install.sh
@@ -8,6 +8,10 @@ Cyan='\033[0;36m'
 White='\033[0;37m'
 NC='\033[0m'
 
+#get the shell configuration file name
+shell_f=$(echo -n "$SHELL" | awk -F / '{print $3}')
+shell_f="${HOME}/.${shell_f}rc"
+
 echo -en "\n    	    	            By: "
 echo -e "$Red mbenkhat $NC\n"
 echo -e "            $Cyan please wait a sec for installation to finish ;)"
@@ -16,12 +20,23 @@ echo -e "                       I'll be happy to help u;)\n $NC"
 
 pip install -r ./42_helper/requirements.txt
 
-echo "alias quote='python3 ~/42_helper/quote.py'
+#test if commands are already installed
+if grep "alias quote='python3 ~/42_helper/quote.py'" <"$shell_f" &>/dev/null \
+&& grep "alias watchmp='python3 ~/42_helper/watch_my_post.py'" <"$shell_f" &>/dev/null \
+&& grep "alias gwatchmp='python3 ~/42_helper/WMP_gui/main.py'" <"$shell_f" &>/dev/null \
+&& grep "alias cluster='python3 ~/42_helper/cluster.py'" <"$shell_f" &>/dev/null;
+then
+	sleep 0.5
+else
+	sleep 0.5
+    echo "alias quote='python3 ~/42_helper/quote.py'
 alias watchmp='python3 ~/42_helper/watch_my_post.py'
 alias gwatchmp='python3 ~/42_helper/WMP_gui/main.py'
-alias cluster='python3 ~/42_helper/cluster.py'" >> ~/.zshrc
+alias cluster='python3 ~/42_helper/cluster.py'" >> $shell_f
+fi
 
 echo -e "\n            $Green Command added: watchmp gwatchmp quote cluster\n"
 echo -e "$Cyan Note: to be able to use watchmp please make to update ~/42_helper/config.py with your data"
 echo "for more info, checkout README.md "
 echo -e "\n                $Red contact me if you need help ;)$NC"
+zsh
